46ccb842f04984f7204001c25dd5f9f17ee8776f
Daniel Río Arizti <drioarizti@cifpfbmoll.eu>
2023-03-17 19:32:51 +0100

    Añadidos comentarios

--- Bloque1/EjercicioAyB.java
+++ Bloque1/EjercicioAyB.java
@@ -5,8 +5,10 @@ public class EjercicioAyB {
     public static void main(String[] args) {
         char[] cuestionarioAlumno = {'a','b','b','d','d','a','c'};
         char[] clavesTest = {'a','c','b','d','d','a','d'};
-        System.out.println(puntosCuestionario(cuestionarioAlumno, clavesTest));
+        System.out.println("Has obtenido " + puntosCuestionario(cuestionarioAlumno, clavesTest)+ " puntos");
     }
+
+    //Método ejercicio a) : recibe dos caracteres, si son iguales devuelve 2(entero), en caso contrario devuelve 0(entero).
     public static int puntosPregunta(char ru, char rc) {
         if (ru == rc) {
             return 2;
@@ -15,6 +17,9 @@ public class EjercicioAyB {
         }
     }
 
+    /*Método ejercicio b) : recibe dos arrays de caracteres una con las respuestas y otra con las claves. No se especifica que pasa
+    cuando los arrays no són del mismo tamaño. Se usa el método del ejercicio a y se devuelve el total de puntos conseguidos al
+    comparar los dos arrays*/
     public static int puntosCuestionario(char[] respuestas, char[] claves) {
         int puntos = 0;
         for (int i = 0; i < respuestas.length; i++) {
--- Bloque1/EjercicioC.java
+++ Bloque1/EjercicioC.java
@@ -1,19 +1,23 @@
 package Programación.JAVA.ExamenRecu2ºTrimestre.Bloque1;
 
 public class EjercicioC {
+
+    //Main para probar el método
     public static void main(String[] args) {
-        int[] dominió = {6,3,3,4,4,5,5,6,6,7,3};
-        if (esPseudoDominio(dominió)) {
-            System.out.println("Es un pseudodominió");
-        } else {
-            System.out.println("No es un pseudodominió");
-        }
+        int[] dominió = {6};
+        //Imprime true o false
+        System.out.println(esPseudoDominio(dominió));
     }
     public static boolean esPseudoDominio(int[] nums) {
-        if (nums.length < 3) {
+        //Si solo hay un par se cumple que es un Pseudodominio
+        if (nums.length == 2) {
             return true;
+        //Si no hay un número par de números no se cumple que es un Pseudodomino
         } else if (nums.length % 2 != 0) {
             return false;
+        /*Con un bucle for que empiece en la segunda posición(nums[1]), que vaya de dos en dos y que no tenga en cuenta las dos últimas
+        posiciones del array, hacemos que se compruebe si el número actual del array no es igual al siguiente, en cuyo caso no es 
+        un Pseudodominio*/
         } else {
             for (int i = 1; i < nums.length -2; i+=2) {
                 if (nums[i] != nums[i+1]) {
@@ -21,6 +25,7 @@ public class EjercicioC {
                 }
             }
         }
+        //En el resto de casos si que será un Pseudodominio
         return true;
     }
 }
